0885bf7e2f7708f353054ec9381b87ade37d0df0
Jad Karaki <jad.karaki@avanade.com>
2018-11-12 14:41:01 +0100

    Added variables and Network Security Group

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "loc" {
+    description = "Azure region"
+    default     =   "westeurope"
+}
+
+variable "tags" {
+    default     = {
+        source  = "citadel"
+        env     = "training"
+    }
+}
