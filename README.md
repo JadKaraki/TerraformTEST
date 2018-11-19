07a2bd34c2e07ae73b2f501efb0f0650942c72a2
Jad Karaki <jad.karaki@avanade.com>
2018-11-19 16:06:12 +0100

    test

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# TerraformTEST
-
-kaddd
+# TerraformTEiffff   
--- samplescript.tf
+++ samplescript.tf
@@ -1,6 +1,9 @@
 
 # VARIABLES 
 
+# test test
+
+
 variable "resource_group" {
   description = "The name of the resource group in which the virtual networks are created"
   default     = "myrg"
